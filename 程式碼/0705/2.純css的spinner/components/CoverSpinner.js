2fcf277cd7ce28c165d1917f16a670631cc405e2
Eddy Chang <eddy@joomla.com.tw>
2021-07-05 14:14:05 +0800

    AD

--- 程式碼/0705/2.純css的spinner/components/CoverSpinner.js
+++ 程式碼/0705/2.純css的spinner/components/CoverSpinner.js
@@ -1,12 +1,11 @@
 import React, { useRef, useEffect } from 'react'
-import '../styles/spinner.scss'
+import './spinner.scss'
 
 function CoverSpinner(props) {
   const spinnerEl = useRef(null)
 
   useEffect(() => {
-    if (props.show)
-      spinnerEl.current.style.display = 'block'
+    if (props.show) spinnerEl.current.style.display = 'block'
   }, [props.show])
 
   return <div id="cover-spin" ref={spinnerEl}></div>
